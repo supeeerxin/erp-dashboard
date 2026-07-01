753992606764395ce575056e491c347a1bff912b
supeeerxin <69573746+supeeerxin@users.noreply.github.com>
2026-07-02 07:34:56 +0800

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -8,10 +8,10 @@ const Layout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 flex">
       <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
       
-      <div className="lg:pl-64 flex flex-col min-h-screen">
+      <div className="flex-1 flex flex-col min-h-screen lg:ml-64">
         <Header onMenuClick={() => setSidebarOpen(true)} />
         
         <main className="flex-1 p-4 md:p-6">
